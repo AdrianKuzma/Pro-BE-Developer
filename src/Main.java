4ce5a3a78c30dac90215d2cc681a030045bb8ef9
AdrianKuzma <beaversvk@gmail.com>
2020-11-13 18:18:26 +0100

    Finished some exercises

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
 
 
 class Main
@@ -8,12 +10,14 @@ class Main
         ArrayList<Integer> list = new ArrayList<>(Arrays.asList(1, 1, 1, 2, 2, 3, 4, 5));
         Person person1 = new Person("John",25,5000);
         Person person2 = new Person("Martin",52,6000);
+        HashSet<Integer> hashbrowns = new HashSet<Integer>(Arrays.asList(1, 1, 1, 2, 2, 3, 4, 5));
+        HashMap<String, Integer> map = new HashMap<>();
+        map.put("Red", 1); map.put("Green", 2); map.put("Black", 3); map.put("White", 4);
+        String k = " Klúč ";  int h = 1;
         exc(list);
         totalBudget(new Person[]{person1,person2});
         contA(new Person[]{person1,person2});
-
     }
-
 
     private static void exc (ArrayList<Integer> list){
         if(list.isEmpty())
@@ -38,11 +42,28 @@ class Main
     
     public static void length(ArrayList <Integer> list){
         list.stream().map(a -> a * list.size()).forEach(System.out::print);
-        }
+    }
+
     public static void totalBudget(Person[] people){
         System.out.println(Arrays.stream(people).map(Person::getBudget).reduce(0, Integer::sum));
     }
+
     public static boolean contA (Person[] people){
         return Arrays.stream(people).anyMatch(person -> person.getName().contains("a"));
+    }
+
+    public static void set (HashSet<Integer> hashbrowns){
+        hashbrowns.forEach(System.out::println);
+    }
+
+    public  static void map (HashMap<String, Integer> map){
+        System.out.println(map);
+    }
+
+    public static void mapadd (HashMap<String, Integer> map, String k, Integer h) {
+        if(!map.containsKey(k)){
+            map.put(k,h);
+        }
+
     }
 }
